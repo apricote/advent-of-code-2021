607bbe918380328e0061611eeb92b53e237ab2f8
Julian Tölle <julian.toelle97@gmail.com>
2021-12-03 16:11:38 +0100

    refactor: read input from files

---
+++ inputs.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func GetInput(path string) string {
+	dat, err := os.ReadFile(path)
+	if err != nil {
+		log.Panicf("error while reading %s", path)
+	}
+
+	return string(dat)
+}
+
+func GetDay1ExampleInput() string {
+	return GetInput("input_1_example.txt")
+}
+
+func GetDay1Input() string {
+	return GetInput("input_1.txt")
+}
+
+func GetDay2ExampleInput() string {
+	return GetInput("input_2_example.txt")
+}
+
+func GetDay2Input() string {
+	return GetInput("input_2.txt")
+}
