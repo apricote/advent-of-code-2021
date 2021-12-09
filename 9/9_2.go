83001df3be0be5a3656f70300a4f04db6c553fde
Julian Tölle <julian.toelle97@gmail.com>
2021-12-09 17:28:49 +0100

    add puzzle 9-2

--- 9/9_2.go
+++ 9/9_2.go
@@ -1,5 +1,62 @@
 package main
 
-func SolveCurrentDayWithTwist(input string) int {
-  return 0
+import "sort"
+
+func (p Point) Equals(p2 Point) bool {
+	return p[0] == p2[0] && p[1] == p2[1]
+}
+
+func GetBasins(input string) int {
+	heightMap := ParseInput(input)
+
+	lowPoints := heightMap.FindLowPoints()
+
+	basins := []int{}
+
+	for _, lowPoint := range lowPoints {
+		basins = append(basins, heightMap.GetBasinSize(lowPoint))
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(basins)))
+
+	return basins[0] * basins[1] * basins[2]
+}
+
+func (hm HeightMap) GetBasinSize(lowPoint Point) int {
+	// List of Points
+	points := []Point{
+		lowPoint,
+	}
+
+	previousLength := 0
+	// While List of Points changes
+	for previousLength < len(points) {
+		previousLength = len(points)
+
+		// Iterate over Points in Basin
+		for _, point := range points {
+
+			adjacentPoints := hm.GetAdjacentPoints(point)
+			for _, adjacentPoint := range adjacentPoints {
+				// Check for adjacent point that are !=9 and not in existing list
+				// add them to the list
+				if hm.Value(adjacentPoint) != 9 && !HasPoint(points, adjacentPoint) {
+					points = append(points, adjacentPoint)
+				}
+			}
+
+		}
+	}
+
+	return len(points)
+}
+
+func HasPoint(points []Point, new Point) bool {
+	for _, point := range points {
+		if point.Equals(new) {
+			return true
+		}
+	}
+
+	return false
 }
--- 9/9_2_test.go
+++ 9/9_2_test.go
@@ -3,22 +3,22 @@ package main
 import (
 	"testing"
 
-	//"github.com/apricote/advent-of-code-2021/util"
+	"github.com/apricote/advent-of-code-2021/util"
 )
 
-func TestSolveCurrentDayWithTwist(t *testing.T) {
+func TestGetBasins(t *testing.T) {
 	type test struct {
 		input string
 		want  int
 	}
 
 	tests := []test{
-		//{input: util.GetExampleInput(), want: 0},
-		//{input: util.GetInput(), want: 0},
+		{input: util.GetExampleInput(), want: 1134},
+		{input: util.GetInput(), want: 847044},
 	}
 
 	for _, tc := range tests {
-		got := SolveCurrentDayWithTwist(tc.input)
+		got := GetBasins(tc.input)
 
 		if tc.want != got {
 			t.Errorf("Expected %d but got %d", tc.want, got)
