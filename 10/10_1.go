1f30699c71bb845044b75727a2afdc58e0169b77
Julian Tölle <julian.toelle97@gmail.com>
2021-12-10 17:21:55 +0100

    add puzzle 10-1

--- 10/10_1.go
+++ 10/10_1.go
@@ -1,5 +1,101 @@
 package main
 
-func SolveCurrentDay(input string) int {
-  return 0
+import (
+	"strings"
+)
+
+type Stack []string
+
+func GetSyntaxErrorScore(input string) int {
+	lines := ParseInput(input)
+
+	points := 0
+
+	for _, line := range lines {
+		illegalCharacter := GetIllegalCharacter(line)
+		points += GetIllegalCharacterPoints(illegalCharacter)
+	}
+
+	return points
+}
+
+func ParseInput(input string) [][]string {
+	parsedLines := [][]string{}
+
+	for _, line := range strings.Split(input, "\n") {
+		parsedLines = append(parsedLines, append([]string{}, strings.Split(line, "")...))
+	}
+
+	return parsedLines
+}
+
+// Returns the first illegal character or "" in case the line is incomplete
+func GetIllegalCharacter(line []string) string {
+	stack := Stack{}
+
+	for _, token := range line {
+		lastElement := ""
+
+		switch token {
+		case "(":
+			fallthrough
+		case "[":
+			fallthrough
+		case "{":
+			fallthrough
+		case "<":
+			stack = append(stack, token)
+		case ")":
+			stack, lastElement = stack.Pop()
+
+			if lastElement != "(" {
+				return token
+			}
+		case "]":
+			stack, lastElement = stack.Pop()
+
+			if lastElement != "[" {
+				return token
+			}
+		case "}":
+			stack, lastElement = stack.Pop()
+
+			if lastElement != "{" {
+				return token
+			}
+		case ">":
+			stack, lastElement = stack.Pop()
+
+			if lastElement != "<" {
+				return token
+			}
+		}
+	}
+
+	return ""
+}
+
+func GetIllegalCharacterPoints(illegalChar string) int {
+	switch illegalChar {
+	case ")":
+		return 3
+	case "]":
+		return 57
+	case "}":
+		return 1197
+	case ">":
+		return 25137
+	default:
+		return 0
+	}
+}
+
+func (s Stack) Pop() (Stack, string) {
+	topIndex := len(s) - 1
+
+	value := s[topIndex]
+	s[topIndex] = ""
+	s = s[:topIndex]
+
+	return s, value
 }
