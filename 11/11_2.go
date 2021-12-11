c5c4e8e6af0425398ce54a1d3f90cbddfbfc95de
Julian Tölle <julian.toelle97@gmail.com>
2021-12-11 15:32:48 +0100

    add puzzle 11-2

--- 11/11_2.go
+++ 11/11_2.go
@@ -1,5 +1,15 @@
 package main
 
-func SolveCurrentDayWithTwist(input string) int {
-  return 0
+func GetFirstDayWithSynchronizedFlashes(input string) int {
+	energyMap := ParseInput(input)
+
+	step := 0
+	flashesInPreviousStep := 0
+
+	for flashesInPreviousStep != EMSize*EMSize {
+		step += 1
+		flashesInPreviousStep = energyMap.SimulateStep()
+	}
+
+	return step
 }
--- 11/11_2_test.go
+++ 11/11_2_test.go
@@ -3,22 +3,22 @@ package main
 import (
 	"testing"
 
-	//"github.com/apricote/advent-of-code-2021/util"
+	"github.com/apricote/advent-of-code-2021/util"
 )
 
-func TestSolveCurrentDayWithTwist(t *testing.T) {
+func TestGetFirstDayWithSynchronizedFlashes(t *testing.T) {
 	type test struct {
 		input string
 		want  int
 	}
 
 	tests := []test{
-		//{input: util.GetExampleInput(), want: 0},
-		//{input: util.GetInput(), want: 0},
+		{input: util.GetExampleInput(), want: 195},
+		{input: util.GetInput(), want: 214},
 	}
 
 	for _, tc := range tests {
-		got := SolveCurrentDayWithTwist(tc.input)
+		got := GetFirstDayWithSynchronizedFlashes(tc.input)
 
 		if tc.want != got {
 			t.Errorf("Expected %d but got %d", tc.want, got)
