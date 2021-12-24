8048230fed0747bb841934ebe631d39edfe3d08a
Julian Tölle <julian.toelle97@gmail.com>
2021-12-24 16:12:27 +0100

    add puzzle 24-1

--- 24/24_1.go
+++ 24/24_1.go
@@ -1,5 +1,42 @@
 package main
 
-func SolveCurrentDay(input string) int {
-  return 0
+import (
+	"log"
+
+	"github.com/apricote/advent-of-code-2021/util"
+)
+
+var (
+	monadInstructions = ParseProgram(util.GetInput())
+)
+
+func MONAD(modelNumber int) bool {
+	inputs := []int{}
+	for modelNumber > 0 {
+		inputs = append(inputs, modelNumber%10)
+		modelNumber /= 10
+	}
+
+	for i := 0; i < len(inputs)/2; i++ {
+		inputs[i], inputs[len(inputs)-1-i] = inputs[len(inputs)-1-i], inputs[i]
+	}
+
+	registers := RunALU(&monadInstructions, inputs)
+	log.Printf("Registers: %v", registers)
+
+	return registers[3] == 0
+}
+
+func BruteForceModelNumber() int {
+	for i := 99999999999999; i >= 11111111111111; i-- {
+		if i%10000 == 0 {
+			log.Printf("Checked %v (%v)", i, (float64(99999999999999-i) / 88888888888888))
+		}
+
+		if MONAD(i) {
+			return i
+		}
+	}
+
+	return 0
 }
--- 24/24_1_test.go
+++ 24/24_1_test.go
@@ -2,26 +2,31 @@ package main
 
 import (
 	"testing"
-
-	"github.com/apricote/advent-of-code-2021/util"
 )
 
-func TestSolveCurrentDay(t *testing.T) {
+func TestMONAD(t *testing.T) {
 	type test struct {
-		input string
-		want  int
+		input int
+		want  bool
 	}
 
 	tests := []test{
-		{input: util.GetExampleInput(), want: 0},
-		//{input: util.GetInput(), want: 0},
+		{input: 13579246899999, want: true},
 	}
 
 	for _, tc := range tests {
-		got := SolveCurrentDay(tc.input)
+		got := MONAD(tc.input)
 
 		if tc.want != got {
-			t.Errorf("Expected %d but got %d", tc.want, got)
+			t.Errorf("Expected %v but got %v", tc.want, got)
 		}
 	}
+}
+
+func TestBruteForceModelNumber(t *testing.T) {
+	modelNumber := BruteForceModelNumber()
+	if modelNumber != 0 {
+		t.Errorf("Expected 0 but got %v", modelNumber)
+	}
+
 }
